c20c3bd7e9e01bf32658ed7321d770b2d31cb5bd
ecogub <69318415+ecogub@users.noreply.github.com>
2022-12-12 16:56:55 -0700

    massive figure improvements
    - monthly flux on ts simualtion plot
    - error % on ts simulation plot

--- paper/hbef_corr_exploration/Ca_correlation_investigation.R
+++ paper/hbef_corr_exploration/Ca_correlation_investigation.R
@@ -1,4 +1,5 @@
 ## based on Audrey's MSdata_formatconvert_phase2.R
+## this can be used to get clean versions of the data for the PHASE 2 MODEL
 library(here)
 library(tidyverse)
 library(feather)
@@ -7,21 +8,26 @@ library(lubridate)
 library(gridExtra)
 library(grid)
 library(lfstat)
-library(ggthemes)
 
-# read in chemistry data ####
+## read in chemistry data
 simple_chem_and_Q <- read_csv(here("paper","hbef_corr_exploration", "HBEFdata_All_2022-11-17.csv")) %>%
     mutate(wy = water_year(date, origin = 'usgs')) %>%
-    filter(site == 'W3',
+    filter(site_code == 'w3',
            wy == 2016)
 
-# run fit Ca and spCond ####
+## run fit likely ions and spCond
+
 complete_ds <- simple_chem_and_Q %>%
-    select(datetime, spCond, Ca) %>%
+    select(datetime, spCond, Ca, Mg, K, Na) %>%
     na.omit(spCond)
 
 summary(lm(Ca~spCond+0, data = complete_ds))
 
+summary(lm(Na~spCond+0, data = complete_ds))
+
+summary(lm(K~spCond+0, data = complete_ds))
+
+summary(lm(Mg~spCond+0, data = complete_ds))
 
 complete_ds %>%
     pivot_longer(cols = -c(datetime, spCond), names_to = 'var', values_to = 'val') %>%
@@ -29,19 +35,10 @@ complete_ds %>%
     geom_point() +
     facet_wrap(~var)
 
-# extract fit ####
+## Ca has the best fit of all of them, with an R-squared of ~0.92 (as does Na)
+## extract fit
+
 fit <- summary(lm(Ca~spCond+0, data = complete_ds))
 fit$coefficients[[1]]
 
 ## Ca in mg/L = spCond*0.06284158
-
-ca_sc_plot <- ggplot(simple_chem_and_Q, aes(x = spCond, y = Ca))+
-                         geom_point()+
-                         geom_smooth(method = 'lm')+
-                         theme_few()+
-                         labs(y = 'Ca (mg/L)',
-                              x = 'SC (uS/cm)')+
-                        theme(text = element_text(size = 20),
-                              plot.title = element_text(size = 30))
-
-ca_sc_plot
